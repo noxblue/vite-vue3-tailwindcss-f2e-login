be13c5e463fd9ef0cea1adb5fd16c32c9ad363b3
Charles Liu <charles.liu@104.com.tw>
2022-12-26 17:44:12 +0800

    Setting the Index and basic page logic

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="flex items-center justify-between p-4 shadow-md">
     <div
-      class="flex items-center rounded border-2 border-dashed border-green-300 bg-amber-50 p-2"
+      class="flex cursor-pointer items-center rounded border-2 border-dashed border-green-300 bg-amber-50 p-2"
+      @click="onIndex"
     >
       <img class="w-5 pr-1" src="/vite.svg" alt="LetsVite" />
       <div class="text-sm font-medium">That's An Account System</div>
     </div>
     <div v-if="isLogin" class="flex">
       <Button class="mr-2" text="會員中心" @click="onAccount" />
+      <Button class="mr-2" text="更改密碼" @click="onPasswordChange" />
       <Button text="登出" @click="onLogout" />
     </div>
     <div v-else class="flex">
@@ -26,19 +28,27 @@ const isLogin = computed(() => false);
 const router = useRouter();
 const route = useRoute();
 
+function onIndex() {
+  router.push({ name: "Index" });
+}
 function onAccount() {
   console.log("account");
+  router.push({ name: "Account" });
+}
+function onPasswordChange() {
+  console.log("PasswordChange");
+  router.push({ name: "ChangePassword" });
 }
 function onLogout() {
   console.log("logout", route);
 }
 function onRegister() {
-  router.push({ name: "Register" });
   console.log("register", router);
+  router.push({ name: "Register" });
 }
 function onLogin() {
-  router.push({ name: "Login" });
   console.log("login", route);
+  router.push({ name: "Login" });
 }
 </script>
 <style scoped></style>
--- src/pages/views/IndexPage.vue
+++ src/pages/views/IndexPage.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Index</div>
+  <div class="flex flex-col items-center justify-center text-center text-xl">
+    <p v-if="isLogin">
+      這是一個首頁<br />好不容易進來了，卻又想出去<br />施主你是想開還是想不開？
+    </p>
+    <p v-else>這是一個首頁<br />看是想登入，還是想註冊<br />要看施主你自己</p>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+const isLogin = computed(() => false);
+</script>
 <style scoped></style>
